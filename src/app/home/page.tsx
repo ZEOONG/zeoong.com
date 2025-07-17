89e704b2c21cb8da0792fd9b475a6b857e6d71b3
minjae0402 <kangjack0402@gmail.com>
2025-07-17 21:19:57 +0900

    Add : Home top bar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,31 @@
+import Image from 'next/image';
+import styles from "@/styles/home.module.scss"
+
 export default function Home() {
     return (
-        <div>
-            <h1>Hello, ZEOONG!</h1>
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="/Eye.png"
+                        alt=""
+                        width={236}
+                        height={143}
+                        style={{
+                            width: '50px',
+                            height: 'auto',
+                        }}
+                        priority
+                    />
+                </div>
+            </div>
+            <header className={styles.header}>
+                <div className={styles.headerlayout}>
+                    <div className={styles.profile}>
+                        <h1>ZEOONG</h1>
+                    </div>
+                </div>
+            </header>
         </div>
     )
 }
