959df7c94a92352da07d825546588c6f537ea5fb
minjae0402 <kangjack0402@gmail.com>
2025-07-21 22:54:43 +0900

    Add : Card OutLineContainer

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -25,52 +25,55 @@ export default function Card() {
 
     return (
         <div className={styles.CardContainer}>
-            <Image
-                src="/background-img.png"
-                alt=""
-                width={1920}
-                height={1080}
-                className={styles.TitleImage}
-            />
-            <div className={styles.CardContent}>
-                <div className={styles.InCardLayout}>
-                    <div className={styles.WritingLayout}>
-                        <h1>Title</h1>
-                        <p>Hello WorldHello WorldHello WorldHello World</p>
-                        <div className={styles.ContentInfo}>
-                            <Link href="https://example.com" className={styles.IconButton}>
-                                <Image
-                                    src="/youtube-svgrepo-com.svg"
-                                    alt="youtube"
-                                    width={24}
-                                    height={24}
-                                />
-                            </Link>
-                            <Link href="https://example.com" className={styles.IconButton}>
-                                <Image
-                                    src="/instagram-svgrepo-com.svg"
-                                    alt="youtube"
-                                    width={24}
-                                    height={24}
-                                />
-                            </Link>
-                            <Link href="https://example.com" className={styles.IconButton}>
-                                <Image
-                                    src="/tiktok-svgrepo-com.svg"
-                                    alt="youtube"
-                                    width={24}
-                                    height={24}
-                                />
-                            </Link>
+            <div className={styles.CardContainerInLine}>
+                <Image
+                    src="/background-img.png"
+                    alt=""
+                    width={1920}
+                    height={1080}
+                    className={styles.TitleImage}
+                />
+                <div className={styles.CardContent}>
+                    <div className={styles.InCardLayout}>
+                        <div className={styles.WritingLayout}>
+                            <h1>Title</h1>
+                            <p>Hello WorldHello WorldHello WorldHello World</p>
+                            <div className={styles.ContentInfo}>
+                                <Link href="https://example.com" className={styles.IconButton}>
+                                    <Image
+                                        src="/youtube-svgrepo-com.svg"
+                                        alt="youtube"
+                                        width={24}
+                                        height={24}
+                                    />
+                                </Link>
+                                <Link href="https://example.com" className={styles.IconButton}>
+                                    <Image
+                                        src="/instagram-svgrepo-com.svg"
+                                        alt="youtube"
+                                        width={24}
+                                        height={24}
+                                    />
+                                </Link>
+                                <Link href="https://example.com" className={styles.IconButton}>
+                                    <Image
+                                        src="/tiktok-svgrepo-com.svg"
+                                        alt="youtube"
+                                        width={24}
+                                        height={24}
+                                    />
+                                </Link>
+                            </div>
                         </div>
-                    </div>
-                    <div className={styles.DownLoadLayout}>
-                        <button onClick={handleDownloadClick} className={`${styles.DownLoadLayoutButton} ${isClicked ? styles.clicked : ''}`}>
-                            <MdDownload className={styles.downloadIcon} />
-                        </button>
+                        <div className={styles.DownLoadLayout}>
+                            <button onClick={handleDownloadClick} className={`${styles.DownLoadLayoutButton} ${isClicked ? styles.clicked : ''}`}>
+                                <MdDownload className={styles.downloadIcon} />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div className={styles.OutInfoContainer}></div>
         </div>
     )
 }
